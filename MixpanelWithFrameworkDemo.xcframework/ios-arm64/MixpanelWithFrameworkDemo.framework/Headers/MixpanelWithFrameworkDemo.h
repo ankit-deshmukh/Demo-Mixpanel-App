b7a38e644ab26c76b2a85eb598310255a99d71ad
ankit-deshmukh <ankit.deshmukh@smallcase.com>
2022-10-03 12:12:51 +0530

    feat: added xcframework containing Mixpanel-swift as a cocoapod dependency

---
+++ MixpanelWithFrameworkDemo.xcframework/ios-arm64/MixpanelWithFrameworkDemo.framework/Headers/MixpanelWithFrameworkDemo.h
@@ -0,0 +1,18 @@
+//
+//  MixpanelWithFrameworkDemo.h
+//  MixpanelWithFrameworkDemo
+//
+//  Created by Ankit Deshmukh on 29/09/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MixpanelWithFrameworkDemo.
+FOUNDATION_EXPORT double MixpanelWithFrameworkDemoVersionNumber;
+
+//! Project version string for MixpanelWithFrameworkDemo.
+FOUNDATION_EXPORT const unsigned char MixpanelWithFrameworkDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MixpanelWithFrameworkDemo/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '15.0'
+
+source 'https://cdn.cocoapods.org'
+source 'https://github.com/ankit-deshmukh/TestMixpanelWithFramework.git'
+
+target 'DemoMPApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for DemoMPApp
+#  pod 'mixpanel-with-framework', '1.0.3'
+pod 'Mixpanel-swift','4.0.2'
+end
